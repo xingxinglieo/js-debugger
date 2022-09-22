81c37e1db2af11adf2025dbaeb172ece7d7d0442
xingyu.hu <954605096@qq.com>
2022-09-23 03:55:03 +0800

    123

--- app/routes/practice/$questionId/Problem/Editor/Monaco/hooks/useCode.ts
+++ app/routes/practice/$questionId/Problem/Editor/Monaco/hooks/useCode.ts
@@ -9,21 +9,34 @@ export default function useCode() {
   const [localCode, setlocalCode] = useLocalStorageState("code" + problem.id, {
     defaultValue:
       problem.template ||
-      `function binarySearch(arr, num) {
-      let left = 0
-      let right = arr.length - 1
-      let middle
-      while (right - left > 1) {
-        middle = Math.floor((left + right) / 2)
-        const now = arr[middle]
-        if (num === now) return middle
-        if (num > now) left = middle
-        else right = middle
+      `// 默认代码 点击编译后即可单步执行
+function MergeSort(arr) {
+  const len = arr.length
+  if (len <= 1) return arr
+  const middle = Math.floor(len / 2)
+  const left = MergeSort(arr.slice(0, middle))
+  const right = MergeSort(arr.slice(middle, len))
+  return merge(left, right)
+  // 核心函数
+  function merge(left, right) {
+    let l = 0 
+    let r = 0
+    let result = []
+    while (l < left.length && r < right.length) {
+      if (left[l] < right[r]) {
+        result.push(left[l])
+        l++
+      } else {
+        result.push(right[r])
+        r++
       }
-      if (arr[right] === num) return right
-      if (arr[left] === num) return left
-      return -1
-    }`
+    }
+    result = result.concat(left.slice(l, left.length))
+    result = result.concat(right.slice(r, right.length))
+    return result
+  }
+}
+MergeSort([10, 9, 8, 7, 6, 1, 2, 3, 4, 5])`
   });
   useEffect(() => {
     if (localCode === "") {
