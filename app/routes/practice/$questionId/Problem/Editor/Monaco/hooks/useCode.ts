ecdf274ddcb2f4da1e01dd533b6f25714d0d87d8
xingyu.hu <954605096@qq.com>
2022-09-22 23:05:24 +0800

    1

--- app/routes/practice/$questionId/Problem/Editor/Monaco/hooks/useCode.ts
+++ app/routes/practice/$questionId/Problem/Editor/Monaco/hooks/useCode.ts
@@ -7,7 +7,23 @@ export default function useCode() {
   const [code, setCode] = useCodeState();
   const [problem] = useProblemState();
   const [localCode, setlocalCode] = useLocalStorageState("code" + problem.id, {
-    defaultValue: problem.template
+    defaultValue:
+      problem.template ||
+      `function binarySearch(arr, num) {
+      let left = 0
+      let right = arr.length - 1
+      let middle
+      while (right - left > 1) {
+        middle = Math.floor((left + right) / 2)
+        const now = arr[middle]
+        if (num === now) return middle
+        if (num > now) left = middle
+        else right = middle
+      }
+      if (arr[right] === num) return right
+      if (arr[left] === num) return left
+      return -1
+    }`
   });
   useEffect(() => {
     if (localCode === "") {
